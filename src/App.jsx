5896262001525b144866a0f9ca1600d782692458
Zacharia Sao <zachariasao@Zacharias-MacBook-Air.local>
2023-02-25 22:51:43 -0500

    add intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Spacer from "./components/Spacer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Spacer size={48} />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import profilePicture from "../assets/profile.jpg";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Container>
+          <Row>
+            <Col md="auto">
+              <Image src={profilePicture} rounded width={500} />
+            </Col>
+            <Col md={6}>
+              <h1>Hi, I'm Zacharia Sao</h1>
+              <h5>Mobile developer</h5>
+              <br />
+              <p>
+                I am a mobile software developer, specializing in Android with a
+                passion for innovative technology. Over the course of six years,
+                I have worked with a variety of companies, from small start-ups
+                to renowned big tech corporations.
+              </p>
+              <br />
+              <h5>Contact</h5>
+              <a href="https://www.linkedin.com/in/zacharia-sao-4aa20850/">
+                Linkedin
+              </a>
+              <p>Email: zachariasao25@gmail.com</p>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Home;
